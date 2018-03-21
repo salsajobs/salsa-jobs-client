9c450dfcb2aa80b6578d574e2a18b34e3b093e57
IagoLast <iago.lastra@gmail.com>
2018-03-21 18:50:48 +0100

    Update build script

--- build.js
+++ build.js
@@ -1,20 +1,17 @@
 const path = require('path');
 const fs = require('fs')
+const production = process.env.PRODUCTION;
 
+// index.html
 const SRC_FILE = path.resolve(__dirname, './src/index.html');
 const OUTPUT_FILE = path.resolve(__dirname, './build/index.html');
-
+// info.html
 const SRC_FILE_INFO = path.resolve(__dirname, './src/info.html');
 const OUTPUT_FILE_INFO = path.resolve(__dirname, './build/info.html');
 
-
-// Read /src/index.html
+// index.html
 const src = fs.readFileSync(SRC_FILE, 'utf8');
-// Replace server URL
-const output = src.replace(/https:\/\/sauce-jobs-staging.herokuapp.com\/jobs/g, process.env.API_URL);
-// Write the output
+const output = src.replace(/https:\/\/sauce-jobs-staging.herokuapp.com/g, process.env.API_URL);
 fs.writeFileSync(OUTPUT_FILE, output, 'utf8');
-
-
-// Landing
+// info.html
 fs.writeFileSync(OUTPUT_FILE_INFO, fs.readFileSync(SRC_FILE_INFO, 'utf8'), 'utf8');
